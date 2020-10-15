266a2f296b1fca71f8653cb621d513a7a881f879
Luis Brazao <luis.ms.brazao@gmail.com>
2020-10-15 18:32:31 +0100

    almost done, just need to finish the bonus and some errors

--- src/movies.js
+++ src/movies.js
@@ -1,15 +1,64 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
+function getAllDirectors(array) {
+
+    let newArray = array.map((movie) => movie.director);
+    return newArray;
+
+}
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+
+function howManyMovies(array) {
+    let spielbergMovies = array.filter(function (element) {
+        return element.director === "Steven Spielberg" && element.genre.includes("Drama");
+    })
+    return spielbergMovies.length;
+}
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
+
+function ratesAverage(array) {
+    if (!array.length) return 0;
+    let total = array.reduce((accumulator, currentValue) => {
+        if(!currentValue.rate){
+            currentValue.rate = 0;
+        }
+        return accumulator + currentValue.rate;
+}, 0);
+    return +(total / array.length).toFixed(2);
+}
+
+
 // Iteration 4: Drama movies - Get the average of Drama Movies
+
+function dramaMoviesRate(array) {
+    let total = array.filter((element) => element.genre.includes("Drama"));
+    if (!total.length) return 0;
+    let sum = total.reduce((accumulator, currentValue) => accumulator + currentValue.rate, 0);
+    return +(sum / total.length).toFixed(2);
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(array) {
+    array.sort((a, b) => a.year - b.year);
+    return array;
+}
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(array) {
+    let sorted = array.sort((a, b) => a.title.localeCompare(b.title));
+    let newArray = [];
+    for (let i = 0; i < 20; i++) {
+        if (i < array.length) {
+            newArray.push(sorted[i].title)
+        }
+    }
+    return newArray;
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
